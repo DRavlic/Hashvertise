f12b60a3ed8029d1b7c61e133452279c0c3c23cd
DRavlic <domagoj.ravlic@gmail.com>
2025-05-27 01:10:03 +0200

    Add UserX model for x info about users

---
+++ backend/src/x/x.model.ts
@@ -0,0 +1,29 @@
+import {
+  prop,
+  getModelForClass,
+  modelOptions,
+  Severity,
+} from "@typegoose/typegoose";
+
+@modelOptions({
+  schemaOptions: {
+    timestamps: true,
+    collection: "UserX",
+  },
+  options: {
+    allowMixed: Severity.ALLOW,
+  },
+})
+export class UserX {
+  @prop({ required: true, unique: true })
+  public xId!: string;
+
+  @prop({ default: true, unique: true })
+  public userName!: string;
+
+  @prop({ default: true })
+  public createdOnX!: Date;
+}
+
+export const UserXModel = getModelForClass(UserX);
+export default UserXModel;
